d7db191f7e7500a5c9e10edb803a5237186bd52b
DhirazX <dhrjpoudyal@gmail.com>
2026-07-26 18:14:08 +0545

    Add tokenizer

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 from pathlib import Path
 import frontmatter
 import numpy as np 
-from sentence_transformers import SentenceTransformers
+from rank_bm25 import BM25Okapi
+from sentence_transformers import SentenceTransformer
 
 
 chunks = []
 # Fetch files and split into chunks
-for file_path in Path(".").rglob("*.md"):
+for file_path in Path("dzcore_dynamics_handbook").rglob("*.md"):
     doc = frontmatter.load(file_path)
-    meta = doc.meta
+    meta = doc.metadata
 
     # Split into Paragraphs first
-    paragraphs = [p.strip() for p in doc.content.split("/n/n") if p.strip() ]
+    paragraphs = [p.strip() for p in doc.content.split("\n\n") if p.strip() ]
 
     # Split to chunks
     for i, para in enumerate(paragraphs):
-        prefix = f"[Dept: {meta.get("department", "N/A")} | Title: {meta.get("title", "N/A")}]"
+        dept = meta.get('department', 'N/A')
+        title = meta.get('title', 'N/A')
+        prefix = f"[Dept: {dept} | Title: {title}] "
 
         chunks.append({
             "chunk_id": f"{meta.get('id', 'DOC')}_c{i}",
@@ -25,3 +28,16 @@ for file_path in Path(".").rglob("*.md"):
 
 
 print(f"Chunks: {len(chunks)}")
+print(chunks[7])
+
+# Extract text only data from chunks
+corpus_text = [c["text"] for c in chunks]
+
+# Tokenize with BM25 for keyword search
+tokenized_corpus = [text.lower().split() for text in corpus_text]
+bm25_index = BM25Okapi(tokenized_corpus)
+
+# Make NumpyMatrix of embeddings for semantic search
+model = SentenceTransformer("all-MiniLM-L6-v2")
+vector_matrix = model.encode(corpus_text, normalize_embeddings=True)
+print(f"Vector matrix shape: {vector_matrix.shape}")
