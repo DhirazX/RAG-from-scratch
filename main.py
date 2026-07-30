513be3c21b3b6033054d3d0ee4a8d8a3dccacf00
DhirazX <dhrjpoudyal@gmail.com>
2026-07-30 18:55:56 +0545

    Add search functions

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import numpy as np
 from rank_bm25 import BM25Okapi
 from sentence_transformers import SentenceTransformer
 
-
 chunks = []
 # Fetch files and split into chunks
 for file_path in Path("dzcore_dynamics_handbook").rglob("*.md"):
@@ -27,8 +26,8 @@ for file_path in Path("dzcore_dynamics_handbook").rglob("*.md"):
         })
 
 
-print(f"Chunks: {len(chunks)}")
-print(chunks[7])
+# print(f"Chunks: {len(chunks)}")
+# print(chunks[7])
 
 # Extract text only data from chunks
 corpus_text = [c["text"] for c in chunks]
@@ -41,3 +40,46 @@ bm25_index = BM25Okapi(tokenized_corpus)
 model = SentenceTransformer("all-MiniLM-L6-v2")
 vector_matrix = model.encode(corpus_text, normalize_embeddings=True)
 print(f"Vector matrix shape: {vector_matrix.shape}")
+
+# Lexical Search with BM25
+def lexical_search(query, top_k = 20):
+    tokenized_query = query.lower().split()
+    scores = bm25_index.get_scores(tokenized_query)
+
+    # Get top_k indices sorted by score (descending)
+    top_indices = np.argsort(scores)[::-1][:top_k]
+    return [(idx, float(scores[idx])) for idx in top_indices]
+
+
+def semnatic_search(query, top_k = 20):
+    query_vector = model.encode(query, normalize_embeddings=True)
+    scores = np.dot(vector_matrix, query_vector )
+
+    top_indices = np.argsort(scores)[::-1][:top_k]
+    return [(idx, float(scores[idx])) for idx in top_indices]
+
+# Reciprocal Rank Fusion (RRF)
+# def hybrid_search(query, top_k = 5): 
+
+#     bm25_results = lexical_search(query)
+#     vector_results = semantic_search(query)
+
+#     rrf_scores={}
+
+
+if __name__ == "__main__":
+    test_query = "What happens if I microwave fish?"
+        
+    # 1. Run Lexical Search
+    bm25_results = lexical_search(test_query, top_k=3)
+    print("///Top 3 BM25 Matches")
+    for idx, score in bm25_results:
+        print(f"Score: {score:.2f}, Chunk ID: {chunks[idx]['chunk_id']}")
+        print(f"Text Snippet: {chunks[idx]['text'][:500]}\n")
+        
+    # 2. Run Dense Search
+    vector_results = semnatic_search(test_query, top_k=3)
+    print("///Top 3 Vector Semantic Matches")
+    for idx, score in vector_results:
+        print(f"Score: {score:.4f}, Chunk ID: {chunks[idx]['chunk_id']}")
+        print(f"Text Snippet: {chunks[idx]['text'][:500]}\n")
